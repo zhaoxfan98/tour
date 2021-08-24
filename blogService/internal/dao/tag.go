e657b7a8c019f69ba17d9f7f5b9fa71820bc5a6a
zhaoxfan98 <zhaoxfan98@gmail.com>
2021-08-24 10:56:51 +0800

    在dao层对Tag对象封装

---
+++ blogService/internal/dao/tag.go
@@ -0,0 +1,45 @@
+package dao
+
+import (
+	"github.com/zhaoxfan98/blog/internal/model"
+	"github.com/zhaoxfan98/blog/pkg/app"
+)
+
+//处理标签模块的 dao 操作
+//在 dao 层进行了数据访问对象的封装，并针对业务所需的字段进行了处理
+
+func (d *Dao) CountTag(name string, state uint8) (int, error) {
+	tag := model.Tag{Name: name, State: state}
+	return tag.Count(d.engine)
+}
+
+func (d *Dao) GetTagList(name string, state uint8, page, pageSize int) ([]*model.Tag, error) {
+	tag := model.Tag{Name: name, State: state}
+	pageOffset := app.GetPageOffset(page, pageSize)
+	return tag.List(d.engine, pageOffset, pageSize)
+}
+
+func (d *Dao) CreateTag(name string, state uint8, createdBy string) error {
+	tag := model.Tag{
+		Name:  name,
+		State: state,
+		Model: &model.Model{CreatedBy: createdBy},
+	}
+
+	return tag.Create(d.engine)
+}
+
+func (d *Dao) UpdateTag(id uint32, name string, state uint8, modifiedBy string) error {
+	tag := model.Tag{
+		Name:  name,
+		State: state,
+		Model: &model.Model{ID: id, ModifiedBy: modifiedBy},
+	}
+
+	return tag.Update(d.engine)
+}
+
+func (d *Dao) DeleteTag(id uint32) error {
+	tag := model.Tag{Model: &model.Model{ID: id}}
+	return tag.Delete(d.engine)
+}
